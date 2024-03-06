7d44cac824144f5c95924993eb68f1afd4f9347b
tyrantgenesis <genesis489449701@gmail.com>
2024-03-06 19:21:24 +0800

    fixed async log

--- src/Enums/ResponseCodeEnum.php
+++ src/Enums/ResponseCodeEnum.php
@@ -2,7 +2,7 @@
 
 namespace TyrantG\LaravelScaffold\Enums;
 
-enum ResponseCodeEnum implements \Throwable: int
+enum ResponseCodeEnum: int
 {
     case RESPONSE_SUCCESS = 0;
     case BAD_REQUEST = 4000;
@@ -13,36 +13,4 @@ enum ResponseCodeEnum implements \Throwable: int
     case REQUEST_SIGNATURE_ERROR = 4103;
     case DATA_HAS_EXISTS = 4433;
     case SERVER_ERROR = 5000;
-
-    public function make(mixed $data = null, ?string $message = null): array
-    {
-        return match ($this) {
-            self::RESPONSE_SUCCESS => http_result($data, self::RESPONSE_SUCCESS, $message ?: '请求成功'),
-            self::BAD_REQUEST => http_result($data, self::BAD_REQUEST, $message ?: '参数错误', 'FAILED'),
-            self::AUTHENTICATION_FAILED => http_result($data, self::AUTHENTICATION_FAILED, $message ?: '鉴权失败', 'FAILED'),
-            self::ROUTE_NOT_FOUND => http_result($data, self::ROUTE_NOT_FOUND, $message ?: '路由不存在', 'FAILED'),
-            self::MODEL_NOT_FOUND => http_result($data, self::MODEL_NOT_FOUND, $message ?: '数据未找到', 'FAILED'),
-            self::REQUEST_TIMESTAMP_EXCEED => http_result($data, self::REQUEST_TIMESTAMP_EXCEED, $message ?: '时间戳校验失败', 'FAILED'),
-            self::REQUEST_SIGNATURE_ERROR => http_result($data, self::REQUEST_SIGNATURE_ERROR, $message ?: '接口签名校验失败', 'FAILED'),
-            self::DATA_HAS_EXISTS => http_result($data, self::DATA_HAS_EXISTS, $message ?: '数据已存在', 'FAILED'),
-            self::SERVER_ERROR => http_result($data, self::SERVER_ERROR, $message ?: '服务器错误', 'FAILED'),
-        };
-    }
-
-    public static function code(): array
-    {
-        return array_column(self::cases(), 'value');
-    }
-
-    public static function response($data, $code, $message, $result = 'SUCCESS'): array
-    {
-        return [
-            'state' => [
-                'code' => $code,
-                'message' => $message,
-                'result' => $result,
-            ],
-            'data' => $data,
-        ];
-    }
 }
